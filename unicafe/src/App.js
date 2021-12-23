1358c34685b379ed0fa8614b4b67a51409921637
Zixin Zhou <zixin.zhou_stella@outlook.com>
2021-12-23 12:07:26 +0200

    part1

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,6 +1,55 @@
 import React, { useState } from 'react'
 
+const Button = (props) =>{
+  return (<button onClick={props.handleClick}>{props.text}</button>)
+}
 
+const StatisticLine = (props) =>{
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  let sum=props.good+props.bad+props.neutral
+  let average=0
+  let positiveRate=0
+  const calAverage = ()=>{
+    average = (props.good-props.bad)/sum
+    positiveRate = props.good/sum*100
+  }
+  if (sum === 0){
+    return (
+      <div>
+        <h1> statistics </h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  else{
+    calAverage()
+    let positiveRateStr = positiveRate.toString()+"%"
+    return (
+      <div>
+        <h1> statistics </h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good}/>
+            <StatisticLine text="neutral" value={props.neutral}/>
+            <StatisticLine text="bad" value={props.bad}/>
+            <StatisticLine text="all" value={sum}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={positiveRateStr}/>
+          </tbody>
+        </table>
+        
+      </div>
+    )
+  }
+}
 
 const App = () => {
   // save number of clicks of each button as separate states
@@ -15,14 +64,10 @@ const App = () => {
   return (
     <div>
       <h1> give feadback </h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-      <h1> statistics </h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      
+      <Button handleClick={handleGoodClick} text="good"/>
+      <Button handleClick={handleNeutralClick} text="neutral"/>
+      <Button handleClick={handleBadClick} text="bad"/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
