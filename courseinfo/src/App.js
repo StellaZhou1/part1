e4f6ad1e8d3272361a02f3ad7e5142448278aceb
Zixin Zhou <zixin.zhou_stella@outlook.com>
2021-12-23 12:28:42 +0200

    part1

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -16,11 +16,9 @@ const Part = (props) => {
 const Content = (props) =>{
   return (
     <div>
-      <p>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-      </p>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
   </div>
   )
 }
